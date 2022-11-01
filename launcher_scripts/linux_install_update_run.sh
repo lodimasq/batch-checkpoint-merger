eb1bb5ef1f220d176380f18b00bbd8b73a6e23b9
Jack Lomas <lodimas123@gmail.com>
2022-11-01 03:59:32 +0000

    file correction

--- launcher_scripts/linux_install_update_run.sh
+++ launcher_scripts/linux_install_update_run.sh
@@ -1,2 +1,4 @@
-venv\Scripts\python.exe -m pip install --upgrade batch_checkpoint_merger
-start venv\Scripts\pythonw.exe -m batch_checkpoint_merger
+#!/usr/bin/env bash
+source "./venv/bin/activate"
+python -m pip install --upgrade batch_checkpoint_merger
+nohup python -m batch_checkpoint_merger >/dev/null 2>&1 &
